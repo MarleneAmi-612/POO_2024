7a19249af992e4b0b949ebf7934d82aecd377f64
MarleneAmi-612 <iamfox.28@gmail.com>
2024-05-20 14:24:27 -0600

    ejercicio5 mejorado

--- Parcial1/6_Ejercicios_repaso_1al5/ejercicio5.py
+++ Parcial1/6_Ejercicios_repaso_1al5/ejercicio5.py
@@ -4,12 +4,10 @@ num1=int(input("Ingrese el primer numero"))
 num2=int(input("Ingrese el segundo numero"))
 
 
-
+num1+=1
 for num in range (num1,num2):
     print(num)
+    
 
-#La verdad es que imprime el primer numero ingresado
-#Hacer cambios y que NO imprima el primer numero
 
-     
 
