c7ed7ac6d43d6aed36422d520667c6914313a58b
MarleneAmi-612 <iamfox.28@gmail.com>
2024-05-21 13:12:47 -0600

    terminado

--- Parcial1/6_Ejercicios_repaso_1al5/ejercicio10.py
+++ Parcial1/6_Ejercicios_repaso_1al5/ejercicio10.py
@@ -1,1 +1,17 @@
 #Crear un programa que solicite la calificacion de 15 alumnos e imprimir en pantalla cuantos aproboron y cuantos no aprobaron
+
+
+reprobad=0
+aprobad=0
+for contador in range(1,16):
+
+     calif=float(input(f"Ingrese la calificación del alumno {contador} (ej. 100): "))
+     
+     if calif<=79:
+        reprobad+=1
+     if calif>=80:
+        aprobad+=1
+
+print(f"{aprobad} aprobados en total")
+print(f"{reprobad} reprobados en total") 
+
