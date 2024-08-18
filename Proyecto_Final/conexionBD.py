bddabd2b71af3f3f4ec6593d57a015595098758b
MarleneAmi-612 <iamfox.28@gmail.com>
2024-08-18 10:11:20 -0600

    Final Commit

--- Proyecto_Final/conexionBD.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def conectar_bd():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="", 
-        database="agencia_service"
-    )
